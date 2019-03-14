f527462a55aea075c9206b57a1252a49308ef5cb
silviajoy <floriosg@gmail.com>
2019-03-14 12:08:09 +0100

    coloured circles excercise

--- src/sketches/sketch02.ts
+++ src/sketches/sketch02.ts
@@ -1,17 +1,20 @@
 const dance = (svg:d3.Selection<d3.BaseType, {}, HTMLElement, any>) => {
     setInterval(() => {
-        let circle = svg.selectAll("circle")
+        
+        const r = Math.floor(Math.random()*255)
+        const g = Math.floor(Math.random()*255)
+        const b = Math.floor(Math.random()*255)
+
+        const color = `rgb(${r}, ${g}, ${b})`
+
+        console.log(color)
         
         svg.append("circle")
-            .attr("cy", 60)
-            .attr("cx", 30)
-            .attr("r",  Math.random()*10)
-            .style("fill", "steelblue");
-
-        circle.attr("cx", function() { return Math.random() * window.innerWidth })
-        circle.attr("cy", function() { return Math.random() * window.innerHeight })
-
-    }, 1000)
+            .attr("cy", Math.random() * window.innerHeight)
+            .attr("cx", Math.random() * window.innerWidth)
+            .attr("r",  Math.random()*100)
+            .style("fill", color);
+    }, 10000)
 }
 
 export default dance
